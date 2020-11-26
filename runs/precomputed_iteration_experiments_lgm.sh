05736043edbb27b0a570abca7e2c6f7422536ae0
Rafael <mostert@strw.leidenuniv.nl>
2020-11-26 18:30:18 +0100

    changed a number of specific experiment yaml files

--- runs/precomputed_iteration_experiments_lgm.sh
+++ runs/precomputed_iteration_experiments_lgm.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 python ../train_lofar.py \
-../configs/lofar_detection/precomputed_iterations_v3_100kconstantLR.yaml /data1/mostertrij #> uitv3.txt &
+../configs/lofar_detection/precomputed_iterations_v3_100kconstantLR.yaml /data1/mostertrij &> uitv3_new.txt &
 
--- runs/precomputed_iteration_experiments_withRot_lgm.sh
+++ runs/precomputed_iteration_experiments_withRot_lgm.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python ../train_lofar.py \
-../configs/lofar_detection/precomputed_iterations_v3_200kconstantLR_withRot.yaml /data1/mostertrij > uitv3_withRot.txt &
+CUDA_VISIBLE_DEVICES=1 python ../train_lofar.py \
+../configs/lofar_detection/precomputed_iterations_v3_200kconstantLR_withRot.yaml /data1/mostertrij &> uitv3_withRot_new.txt &
 
