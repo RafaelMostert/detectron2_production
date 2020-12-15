1fd7bbb571b00598b42c34c7e97ae2c29def542c
Rafael Mostert <mostert@strw.leidenuniv.nl>
2020-12-15 10:23:28 +0100

    added test scripts

--- detectron2/evaluation/lofar_evaluation.py
+++ detectron2/evaluation/lofar_evaluation.py
@@ -89,11 +89,16 @@ class LOFAREvaluator(DatasetEvaluator):
             if self.inference_only:
                 prediction = {"image_id": input["image_id"], "file_name":input["file_name"],
                     "focussed_comp":input["focussed_comp"],"related_comp":input["related_comp"],
-                    "unrelated_comp":input["unrelated_comp"],"unrelated_names":input["unrelated_names"]}
+                    "unrelated_comp":input["unrelated_comp"],
+                    "related_unresolved":input["related_unresolved"],
+                    "unrelated_unresolved":input["unrelated_unresolved"],
+                    "unrelated_names":input["unrelated_names"]}
             else:
                 prediction = {"image_id": input["image_id"], "file_name":input["file_name"],
                     "focussed_comp":input["focussed_comp"],"related_comp":input["related_comp"],
-                    "unrelated_comp":input["unrelated_comp"]}
+                    "unrelated_comp":input["unrelated_comp"],
+                    "related_unresolved":input["related_unresolved"],
+                    "unrelated_unresolved":input["unrelated_unresolved"]}
 
             if "instances" in output:
                 instances = output["instances"].to(self._cpu_device)#.numpy()
@@ -106,10 +111,12 @@ class LOFAREvaluator(DatasetEvaluator):
         self.focussed_comps = [p["focussed_comp"] for p in self._predictions]
         self.related_comps = [p["related_comp"] for p in self._predictions]
         self.unrelated_comps = [p["unrelated_comp"] for p in self._predictions]
-        #self.related_comps = [p["related_comp"] if len(p["related_comp"])>0 else [[],[]] 
-        #        for p in self._predictions]
-        #self.unrelated_comps = [p["unrelated_comp"] if len(p["unrelated_comp"])>0 else [[],[]] 
-        #        for p in self._predictions]
+        self.related_unresolved = [p["related_unresolved"] for p in self._predictions]
+        self.unrelated_unresolved = [p["unrelated_unresolved"] for p in self._predictions]
+        print("related unresolved:", self.related_unresolved[0])
+        print("unrelated unresolved:", self.unrelated_unresolved[0])
+        sdfsdf
+
         if self.inference_only:
             self.unrelated_names = [p["unrelated_names"] if len(p["unrelated_names"])>0 else [] 
                 for p in self._predictions]
