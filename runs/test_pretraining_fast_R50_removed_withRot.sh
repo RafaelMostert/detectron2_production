f63cc28ec168411a489cb7beb115564289f308e6
Rafael <mostert@strw.leidenuniv.nl>
2021-11-04 07:23:28 -0400

    added pretrained weights testing

---
+++ runs/test_pretraining_fast_R50_removed_withRot.sh
@@ -0,0 +1,22 @@
+# When executed, the code below will train our model using a variety of settings and implemented
+# features.
+
+val="uLB300_fast_R50_pretrained_removed_withRot_constantLR" 
+
+seed=1
+CUDA_VISIBLE_DEVICES=0 python ../train_lofar.py \
+        ../configs/lofar_detection/$val".yaml" \
+        /data1/mostertrij $seed &> \
+        logs/$val"_seed"$seed".txt" & 
+sleep 90
+seed=2
+CUDA_VISIBLE_DEVICES=1 python ../train_lofar.py \
+        ../configs/lofar_detection/$val".yaml" \
+        /data1/mostertrij $seed &> \
+        logs/$val"_seed"$seed".txt" & 
+sleep 90
+seed=3
+CUDA_VISIBLE_DEVICES=2 python ../train_lofar.py \
+        ../configs/lofar_detection/$val".yaml" \
+        /data1/mostertrij $seed &> \
+        logs/$val"_seed"$seed".txt" 
